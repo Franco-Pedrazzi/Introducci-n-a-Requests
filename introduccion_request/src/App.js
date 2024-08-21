86e13b9b075dd856ee4a7d37fc47120b42a0a3b7
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-08-21 10:54:27 -0300

    termine la actividad Codigos de requests y manejo de errores

--- introduccion_request/src/App.js
+++ introduccion_request/src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 import User from './User.js';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [codigo, setCodigo] = useState("")
   const [pais, setPais] = useState("")
+  const [error,setError]=useState("")
   const handleCodigo = (event) => {
     setCodigo(event.target.value)
   }
   const handleSubmit = (event) => {
     event.preventDefault();
     fetch('https://restcountries.com/v3.1/name/' + codigo)
-      .then(response => response.json())
-      .then(data => { setPais(data[0]) })
+      .then(response => {
+        if(response.ok){
+           return response.json()
+          }
+        else{
+          setError(`ERROR:${response.status}`)
+          setPais("")
+          throw new Error(`ERROR:${response.status}`);
+        }
+       })
+      .then(data => setPais(data[0]))
+      .catch(error => console.log(error));
   }
-
-
   return (
     <div className="App">
 
@@ -31,6 +40,7 @@ function App() {
         {pais && <h1>{pais["name"]["common"]}</h1>}
         {pais && <img src={pais["flags"]["png"]}></img>}
         {pais && <h2>Capital: {pais["capital"][0]}</h2>}
+        {error && <h1>{error}</h1>}
         <h1>Random User</h1>
         
         {<User /> }
--- introduccion_request/src/User.js
+++ introduccion_request/src/User.js
@@ -1,15 +1,32 @@
 import { useState } from 'react';
 import './App.css';
+
 function User() {
-  const [id,setId]=useState(Math.ceil(Math.random() * 1000)) 
-  const ChangeUser = () => {
-    setId(Math.ceil(Math.random() * 1000));
-    
-  }
+  const [ids, setIds] = useState([]);
+  const [pocicion, setPocicion] = useState(0);
+
+  const AddUser = () => {
+    const newValue = [Math.ceil(Math.random() * 1000), pocicion];
+    setIds([...ids, newValue]);
+    setPocicion(pocicion + 1);
+  };
+
+  const deleteUser = (pocicionParaEliminar) => {
+    const updatedIds = ids.filter(id => id[1] !== pocicionParaEliminar);
+    setIds(updatedIds);
+  };
+
   return (
     <div className="App">
-      <button onClick={ChangeUser}>randomUser</button>
-        <img src={`https://robohash.org/${id}`}/>
+      <button onClick={AddUser}>randomUser</button>
+      <ul>
+        {ids.map((id) => (
+          <li>
+            <img src={`https://robohash.org/${id[0]}`}/>
+            <button onClick={() => deleteUser(id[1])}>delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
